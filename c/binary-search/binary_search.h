a827b633b467a6ec8f5e0bf9a4e44c9c099a363a
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-08-06 13:20:23 -0700

    Add Binary Search Seems to be running infinite loop

---
+++ c/binary-search/binary_search.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int *binary_search(int value, const int *arr, size_t length);
+
+#endif
